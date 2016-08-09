a6c9d9d5d8731e302f9486d584de8963132ee200
erikvw <ew2789@gmail.com>
2016-08-09 20:18:59 +0200

    update app config

--- edc_audio_recording/apps.py
+++ edc_audio_recording/apps.py
@@ -1,19 +1,19 @@
 import sys
 import sounddevice as sd
 
-from django.apps import AppConfig
+from django.apps import AppConfig as DjangoAppConfig
 from django.core.management.color import color_style
 
 
-class EdcAudioRecordingAppConfig(AppConfig):
+class AppConfig(DjangoAppConfig):
     name = 'edc_audio_recording'
     verbose_name = 'Audio Recordings'
     audio_file_extensions = ['.npz']
 
     def ready(self):
         style = color_style()
+        sys.stdout.write('Loading {} ...\n'.format(self.verbose_name))
         try:
-            sys.stdout.write('Loading {} ...\n'.format(self.verbose_name))
             device = sd.query_devices(None, 'input')
             sys.stdout.write(' * found audio input device \'{}\''.format(device['name']) + '\n')
         except (sd.PortAudioError, ValueError) as e:
@@ -21,3 +21,4 @@ class EdcAudioRecordingAppConfig(AppConfig):
                 style.WARNING(
                     'Warning: no compatible audio input devices found. '
                     'Audio recording will be disabled. Got {}\n'.format(str(e))))
+        sys.stdout.write(' Done loading {}.\n'.format(self.verbose_name))
